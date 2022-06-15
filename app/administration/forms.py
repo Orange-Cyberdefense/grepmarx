ae0da5cced660cc3b9fa2f74954a3b872cfe77ea
dirbuster <dirbuster@WX-OR6259722.ocdpentest>
2022-06-15 11:39:42 +0200

    wt-form 3.0.0 migration

--- app/administration/forms.py
+++ app/administration/forms.py
@@ -9,17 +9,17 @@ from pickle import FALSE
 from tkinter.tix import Select
 from xmlrpc.client import Boolean
 from flask_wtf import FlaskForm
-from wtforms import TextField, PasswordField, HiddenField, SelectField
+from wtforms import StringField, PasswordField, HiddenField, SelectField
 from wtforms.fields.simple import TextAreaField
 from wtforms.validators import Email, DataRequired, Regexp
 
 
 class UserForm(FlaskForm):
     id = HiddenField("User id")
-    username = TextField("Username", id="user-username", validators=[DataRequired()])
-    first_name = TextField("First name", id="user-first-name")
-    last_name = TextField("Last name", id="user-last-name")
-    email = TextField("Email", id="user-email", validators=[DataRequired(), Email()])
+    username = StringField("Username", id="user-username", validators=[DataRequired()])
+    first_name = StringField("First name", id="user-first-name")
+    last_name = StringField("Last name", id="user-last-name")
+    email = StringField("Email", id="user-email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", id="user-password")
     password_confirm = PasswordField("Confirm password", id="user-confirm-password")
     roles = [(int(0), 'user'),(int(1),'admin')]
@@ -27,8 +27,8 @@ class UserForm(FlaskForm):
 
 class RepositoryForm(FlaskForm):
     id = HiddenField("Repository id")
-    name = TextField("Repository name", id="repo-name", validators=[DataRequired(), Regexp('^[a-zA-Z0-9-_]+$', message="Repository name must contain only letters, numbers, dash (-) or underscore (_) characters"),])
+    name = StringField("Repository name", id="repo-name", validators=[DataRequired(), Regexp('^[a-zA-Z0-9-_]+$', message="Repository name must contain only letters, numbers, dash (-) or underscore (_) characters"),])
     description = TextAreaField("Repository description", id="repo-description")
-    uri = TextField("Repository URI", id="repo-uri", validators=[DataRequired()])
-    git_username = TextField("Git username", id="repo-username")
-    git_token = TextField("Git Acess Token", id="repo-token")
+    uri = StringField("Repository URI", id="repo-uri", validators=[DataRequired()])
+    git_username = StringField("Git username", id="repo-username")
+    git_token = StringField("Git Acess Token", id="repo-token")
