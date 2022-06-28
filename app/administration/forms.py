8db64800af2a1c2fa20600993405388ca5e063ee
dirbuster <dirbuster@WX-OR6259722.ocdpentest>
2022-06-28 15:06:30 +0200

    wtforms correction

--- app/administration/forms.py
+++ app/administration/forms.py
@@ -9,7 +9,7 @@ from pickle import FALSE
 from tkinter.tix import Select
 from xmlrpc.client import Boolean
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, HiddenField, SelectField
+from wtforms import TextField, PasswordField, HiddenField, SelectField
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms.fields.simple import TextAreaField
 from wtforms.validators import Email, DataRequired, Regexp
@@ -17,10 +17,10 @@ from wtforms.validators import Email, DataRequired, Regexp
 
 class UserForm(FlaskForm):
     id = HiddenField("User id")
-    username = StringField("Username", id="user-username", validators=[DataRequired()])
-    first_name = StringField("First name", id="user-first-name")
-    last_name = StringField("Last name", id="user-last-name")
-    email = StringField("Email", id="user-email", validators=[DataRequired(), Email()])
+    username = TextField("Username", id="user-username", validators=[DataRequired()])
+    first_name = TextField("First name", id="user-first-name")
+    last_name = TextField("Last name", id="user-last-name")
+    email = TextField("Email", id="user-email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", id="user-password")
     password_confirm = PasswordField("Confirm password", id="user-confirm-password")
     roles = [(int(0), 'user'),(int(1),'admin')]
@@ -28,20 +28,20 @@ class UserForm(FlaskForm):
 
 class RepositoryForm(FlaskForm):
     id = HiddenField("Repository id")
-    name = StringField("Repository name", id="repo-name", validators=[DataRequired(), Regexp('^[a-zA-Z0-9-_]+$', message="Repository name must contain only letters, numbers, dash (-) or underscore (_) characters"),])
+    name = TextField("Repository name", id="repo-name", validators=[DataRequired(), Regexp('^[a-zA-Z0-9-_]+$', message="Repository name must contain only letters, numbers, dash (-) or underscore (_) characters"),])
     description = TextAreaField("Repository description", id="repo-description")
-    uri = StringField("Repository URI", id="repo-uri", validators=[DataRequired()])
-    git_username = StringField("Git username", id="repo-username")
-    git_token = StringField("Git Acess Token", id="repo-token")
+    uri = TextField("Repository URI", id="repo-uri", validators=[DataRequired()])
+    git_username = TextField("Git username", id="repo-username")
+    git_token = TextField("Git Acess Token", id="repo-token")
 
 class LdapForm(FlaskForm):
     id = HiddenField("Ldap id")
-    display_name = StringField("Display name", id="ldap-name", validators=[DataRequired(), Regexp('^[a-zA-Z0-9-_]+$', message="Repository name must contain only letters, numbers, dash (-) or underscore (_) characters")])
+    display_name = TextField("Display name", id="ldap-name", validators=[DataRequired(), Regexp('^[a-zA-Z0-9-_]+$', message="Repository name must contain only letters, numbers, dash (-) or underscore (_) characters")])
     password = PasswordField("Password", id="ldap-password")
-    url = StringField("LDAP URL", id="ldap-url", validators=[DataRequired()])
-    admin_bind_dn = StringField("Admin Bind DN", id="ldap-dn", validators=[DataRequired()])
-    search_base = StringField("Search base", id="ldap-search", validators=[DataRequired()])
-    searchfilter = StringField("Search Filter", id="ldap-filter", validators=[DataRequired()])
+    url = TextField("LDAP URL", id="ldap-url", validators=[DataRequired()])
+    admin_bind_dn = TextField("Admin Bind DN", id="ldap-dn", validators=[DataRequired()])
+    search_base = TextField("Search base", id="ldap-search", validators=[DataRequired()])
+    searchfilter = TextField("Search Filter", id="ldap-filter", validators=[DataRequired()])
     tls_cert = FileField("Tls certificate", id="ldap-cert", validators=[FileRequired(), FileAllowed(['cert'], 'certr only')])
 
 
