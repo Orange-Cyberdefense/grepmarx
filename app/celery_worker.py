b106bc921e3fb3a9dfc22d38c1f5d7a3adf493a4
dirbuster <dirbuster@WX-OR6259722.ocdpentest>
2022-06-29 16:04:52 +0200

    fixed grepmarx server

--- app/celery_worker.py
+++ app/celery_worker.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from app import create_app
 from sys import exit
 from decouple import config
 from app.config import config_dict
 
-
+DEBUG = True
 DEBUG = config('DEBUG', default=True, cast=bool)
 get_config_mode = 'Debug' if DEBUG else 'Production'
 try:
-    app_config = config_dict[get_config_mode.capitalize()]
+    app_config = config_dict[get_config_mode]
 except KeyError:
     exit('Error: Invalid <config_mode>. Expected values [Debug, Production] ')
 
